f1ba28cfd2241fbd738c2d0fa03bd770be1bebf3
Mimi <mimi73727170@gmail.com>
2026-07-17 17:55:12 +0530

    Add files via upload

---
+++ Variables,Input,Output&Data tyye Revision.py
@@ -0,0 +1,12 @@
+#Practice Task 1
+name = input("Enter your name :")
+age = eval(input("Enter your age :"))
+fav = input("Enter favourite subject :")
+print(name)
+print(age)
+print(fav)
+
+#Practice Task 2
+name1 = input("Enter your first name :")
+name2 = input("Enter your second name :")
+print((name1)," ",(name2))
