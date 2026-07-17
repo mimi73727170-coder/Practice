15b63956783041151d1cef468c11831a5e44d4cd
Mimi <mimi73727170@gmail.com>
2026-07-17 18:10:16 +0530

    Update Variables,Input,Output&Data tyye Revision.py

--- Variables,Input,Output&Data tyye Revision.py
+++ Variables,Input,Output&Data tyye Revision.py
@@ -10,3 +10,13 @@ print(fav)
 name1 = input("Enter your first name :")
 name2 = input("Enter your second name :")
 print((name1)," ",(name2))
+
+#Practice Task 3
+name3 = input("Enter your name :")
+phy = input(int("Enter physics marks :"))
+chem = input(int("Enter chemistry marks :"))
+math = input(int("Enter math marks :"))
+print(name3)
+print(phy)
+print(chem)
+print(math)
